7217fe240e7e7f8936da09c78a8f3bb8e63d2cae
Tiago Barbosa <tiago@farias.cc>
2021-07-25 14:56:08 -0300

    Work with Files: File and FileInfo

---
+++ FileFileInfo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace FileFileInfo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string sourcePath = @"/Users/tiagobarbosa/Documents/Estudos/Programação/Udemy/C# - COMPLETO/ProjectsCSharpStudy/FilesTest/file1.txt";
+            string targetPath = @"/Users/tiagobarbosa/Documents/Estudos/Programação/Udemy/C# - COMPLETO/ProjectsCSharpStudy/FilesTest/file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                Console.WriteLine("File copy sucess!");
+                Console.WriteLine();
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.Write(line);
+                    Console.WriteLine($" - Qtd characteres on line: {line.Length}");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocorrer");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
