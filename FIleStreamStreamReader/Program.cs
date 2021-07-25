2b7fa1f38c6079c6373bea60408da5865e43e059
Tiago Barbosa <tiago@farias.cc>
2021-07-25 15:20:36 -0300

    Work with Files: FileStream and StreamReader

---
+++ FIleStreamStreamReader/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace FIleStreamStreamReader
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"/Users/tiagobarbosa/Documents/Estudos/Programação/Udemy/C# - COMPLETO/ProjectsCSharpStudy/FilesTest/file1.txt";
+
+            StreamReader streamReader = null;
+
+            try
+            {
+                streamReader = File.OpenText(path);
+                while (!streamReader.EndOfStream)
+                {
+                    string line = streamReader.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (streamReader != null)
+                {
+                    streamReader.Close();
+                }
+            }
+        }
+    }
+}
