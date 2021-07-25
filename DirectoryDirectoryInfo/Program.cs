501756bec7ba86f6ae044d7384cb3a9d6b532b82
Tiago Barbosa <tiago@farias.cc>
2021-07-25 15:59:47 -0300

    Working with Files: Directory and DirectoryInfo

---
+++ DirectoryDirectoryInfo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+//using System.Collections.Generic;
+
+namespace DirectoryDirectoryInfo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"/Users/tiagobarbosa/Documents/Estudos/Programação/Udemy/C# - COMPLETO/ProjectsCSharpStudy/FilesTest";
+
+            try
+            {
+                //IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS:");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES:");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                Console.WriteLine();
+
+                Console.WriteLine($"New folder created => {Directory.CreateDirectory(path + "/newfolder")}");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
