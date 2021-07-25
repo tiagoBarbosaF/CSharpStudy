34896e4031c8883168ed017656f5328726d96179
Tiago Barbosa <tiago@farias.cc>
2021-07-25 16:11:58 -0300

    working with files: path

---
+++ PathStudy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace PathStudy
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"/Users/tiagobarbosa/Documents/Estudos/Programação/Udemy/C# - COMPLETO/ProjectsCSharpStudy/FilesTest/file1.txt";
+
+            Console.WriteLine($"DirectorySeparatorChar => {Path.DirectorySeparatorChar}");
+            Console.WriteLine($"PathSeparator => {Path.PathSeparator}");
+            Console.WriteLine($"GetDirectoryName => {Path.GetDirectoryName(path)}");
+            Console.WriteLine($"GetFileName => {Path.GetFileName(path)}");
+            Console.WriteLine($"GetExtension => {Path.GetExtension(path)}");
+            Console.WriteLine($"GetFileNameWithoutExtension => {Path.GetFileNameWithoutExtension(path)}");
+            Console.WriteLine($"GetFullPath => {Path.GetFullPath(path)}");
+            Console.WriteLine($"GetTempPath => {Path.GetTempPath()}");
+        }
+    }
+}
