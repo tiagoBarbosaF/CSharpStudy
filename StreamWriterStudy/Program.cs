22dcad3a6ac0a4cf946f7692af0edcc421345905
Tiago Barbosa <tiago@farias.cc>
2021-07-25 15:39:37 -0300

    Working with Files: StreamWriter

---
+++ StreamWriterStudy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace StreamWriterStudy
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string sourcepath = @"/Users/tiagobarbosa/Documents/Estudos/Programação/Udemy/C# - COMPLETO/ProjectsCSharpStudy/FilesTest/file1.txt";
+            string targetpath = @"/Users/tiagobarbosa/Documents/Estudos/Programação/Udemy/C# - COMPLETO/ProjectsCSharpStudy/FilesTest/file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcepath);
+
+                using (StreamWriter streamWriter = File.AppendText(targetpath))
+                {
+                    foreach (string line in lines)
+                    {
+                        streamWriter.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
