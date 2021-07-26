ca80d5195d2572dfa2f6e877ec9659ad296f50b5
Tiago Barbosa <tiago@farias.cc>
2021-07-26 17:39:11 -0300

    Interface: IComparable

---
+++ InterfaceIComparable/InterfaceIComparable/Entities/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace InterfaceIComparable.Entities
+{
+    class Employee : IComparable
+    {
+        public string name { get; set; }
+        public double salary { get; set; }
+
+        public Employee(string csvEmployee)
+        {
+            string[] vect = csvEmployee.Split(',');
+            name = vect[0];
+            salary = double.Parse(vect[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {name}, Salary: {salary.ToString("F2",CultureInfo.InvariantCulture)}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Employee))
+            {
+                throw new ArgumentException("Comparing erros: argument is not valid.");
+            }
+
+            Employee other = obj as Employee;
+            return name.CompareTo(other.name);
+        }
+    }
+}
---
+++ InterfaceIComparable/InterfaceIComparable/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using InterfaceIComparable.Entities;
+
+namespace InterfaceIComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string path = @"C:\Users\antonio.farias\Documents\Tiago GitHub\CSharpStudy\CSharpStudy\FilesTest\in.txt";
+            string path = @"C:\Users\antonio.farias\Documents\Tiago GitHub\CSharpStudy\CSharpStudy\FilesTest\in2.txt";
+
+            try
+            {
+                using(StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+
+                    foreach(Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+
+            } catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
