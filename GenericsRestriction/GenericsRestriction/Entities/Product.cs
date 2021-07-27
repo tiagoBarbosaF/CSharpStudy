540f7bfb426cbb29fc8ce803dcc1494021ef2c69
Tiago Barbosa <tiago@farias.cc>
2021-07-27 10:29:10 -0300

    Generics restriction

---
+++ GenericsRestriction/GenericsRestriction/Entities/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsRestriction.Entities
+{
+    class Product : IComparable
+    {
+        public string name { get; set; }
+        public double price { get; set; }
+
+        public Product(string name, double price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{name}, {price.ToString("F2",CultureInfo.InvariantCulture)}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Product))
+            {
+                throw new ArgumentException("Comparing error: argument is not a Product.");
+            }
+
+            Product other = obj as Product;
+            return price.CompareTo(other.price);
+        }
+    }
+}
--- GenericsStudy/GenericsStudy/Program.cs
+++ GenericsStudy/GenericsStudy/Program.cs
@@ -6,7 +6,7 @@ namespace GenericsStudy
     {
         static void Main(string[] args)
         {
-            PrintService printService = new PrintService();
+            PrintService<int> printService = new PrintService<int>();
 
             Console.Write("How many values? ");
             int n = int.Parse(Console.ReadLine());
