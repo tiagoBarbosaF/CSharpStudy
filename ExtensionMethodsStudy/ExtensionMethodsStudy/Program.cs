f17d14a1d69bc54e13832608f41f03f0b93e5a8b
Tiago Barbosa <tiago@farias.cc>
2021-07-27 16:36:18 -0300

    Extension methods

---
+++ ExtensionMethodsStudy/ExtensionMethodsStudy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExtensionMethodsStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2021, 7, 20, 8, 14, 35);
+            Console.WriteLine($"DateTime => {dt.ElapsedTime()}");
+
+            Console.WriteLine();
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
