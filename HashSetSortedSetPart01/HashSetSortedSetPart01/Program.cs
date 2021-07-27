2479ff6b3b58aa16d5ea3f72fd25ce80ef857d88
Tiago Barbosa <tiago@farias.cc>
2021-07-27 11:25:31 -0300

    HashSet and SortedSet part#1

---
+++ HashSetSortedSetPart01/HashSetSortedSetPart01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSetSortedSetPart01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("TV"));
+            Console.WriteLine(set.Contains("Computer"));
+            Console.WriteLine();
+
+            foreach(string p in set)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
