8b9dfd14dfb730526e0acfa8624d05d91e1ff4c8
Tiago Barbosa <tiago@farias.cc>
2021-07-27 11:05:58 -0300

    GetHashCode and Equals

---
+++ GetHashCodeEquals/GetHashCodeEquals/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using GetHashCodeEquals.Entities;
+
+namespace GetHashCodeEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client() { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client() { Name = "Tiago", Email = "tiago@gmail.com" };
+            Client c = new Client() { Name = "Tiago", Email = "maria@gmail.com" };
+
+            Console.WriteLine($"Client a equals b? {a.Equals(b)}");
+            Console.WriteLine($"Client a equals c? {a.Equals(c)}");
+            Console.WriteLine($"Client a == b? {a == b}");
+            Console.WriteLine($"Client a == c? {a == c}");
+            Console.WriteLine($"Client a GetHashCode? {a.GetHashCode()}");
+            Console.WriteLine($"Client b GetHashCode? {b.GetHashCode()}");
+            Console.WriteLine($"Client c GetHashCode? {c.GetHashCode()}");
+        }
+    }
+}
