07941a0912b39f3551e40647dcb57f50722ab223
Tiago Barbosa <tiago@farias.cc>
2021-07-27 13:27:52 -0300

    HashCollection

---
+++ HashCollection/HashCollection/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using HashCollection.Entities;
+
+namespace HashCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Product> a = new HashSet<Product>();
+            a.Add(new Product("TV", 900.0));
+            a.Add(new Product("Notebook", 1200.0));
+
+            HashSet<Point> b = new HashSet<Point>();
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
+
+            Product prod = new Product("Notebook", 1200.0);
+            Console.WriteLine($"Struct => {a.Contains(prod)}");
+
+            Point p = new Point(5, 10);
+            Console.WriteLine($"Class => {b.Contains(p)}");
+        }
+    }
+}
