95529e16022fbd947603c3b8451cecd117d20457
Tiago Barbosa <tiago@farias.cc>
2021-07-27 09:28:49 -0300

    Generics study part #1

---
+++ GenericsStudy/GenericsStudy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GenericsStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{i + 1}# value: ");
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine($"First: {printService.First()}");
+        }
+    }
+}
