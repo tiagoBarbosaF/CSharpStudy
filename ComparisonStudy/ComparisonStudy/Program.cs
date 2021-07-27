f77837d7c3a13cc5fafa94a9b482afb824c81994
Tiago Barbosa <tiago@farias.cc>
2021-07-27 17:31:12 -0300

    Comparison and Lambda

---
+++ ComparisonStudy/ComparisonStudy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using ComparisonStudy.Entities;
+
+namespace ComparisonStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+            //list.Sort((p1,p2) => p1.Price.CompareTo(p2.Price));
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
