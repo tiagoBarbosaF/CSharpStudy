4e00001669e3b02c1fad73955f1bc2b7a0a7574e
Tiago Barbosa <tiago@farias.cc>
2021-07-28 15:17:08 -0300

    LINQ with SQL notation

--- LinqWithLambda/LinqWithLambda/Program.cs
+++ LinqWithLambda/LinqWithLambda/Program.cs
@@ -37,62 +37,36 @@ namespace LinqWithLambda
                 new Product() { Id = 11, Name = "Level", Price = 70.0, Category = c1}
             };
 
-            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+            //var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+            var r1 =
+                from p in products
+                where p.Category.Tier == 1 && p.Price < 900.0
+                select p;
             Print("Tier 1 and Price < 900:", r1);
 
-            var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            //var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            var r2 = from p in products where p.Category.Name == "Tools" select p.Name;
             Print("Names of products from Tools:", r2);
 
-            var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            //var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            var r3 = from p in products where p.Name[0] == 'C' select new { p.Name, p.Price, CategoryName = p.Category.Name };
             Print("Names started with 'C' and anonymous objects", r3);
 
-            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            //var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            var r4 = from p in products where p.Category.Tier == 1 orderby p.Name orderby p.Price select p;
             Print("Product with Category Tier 1 ordered by Price and Name:", r4);
 
-            var r5 = r4.Skip(2).Take(4);
+            //var r5 = r4.Skip(2).Take(4);
+            var r5 = (from p in r4 select p).ToList().Skip(2).Take(4);
             Print("Product with Category Tier 1 ordered by Price and Name, Skip 2 and Take 4:", r5);
 
-            var r6 = products.FirstOrDefault();
-            Console.WriteLine("First or Default test1: " + r6);
-
-            var r7 = products.Where(p => p.Price > 3000.0).FirstOrDefault();
-            Console.WriteLine("First or Default test2: " + r7);
             Console.WriteLine();
-
-            var r8 = products.Where(p => p.Id == 3).SingleOrDefault();
-            Console.WriteLine("Single or default test1: " + r8);
-
-            var r9 = products.Where(p => p.Id == 30).SingleOrDefault();
-            Console.WriteLine("Single or default test2: " + r9);
-
-            var r10 = products.Max(p => p.Price);
-            Console.WriteLine("Max price: " + r10);
-
-            var r11 = products.Min(p => p.Price);
-            Console.WriteLine("Min price: " + r11);
-
-            var r12 = products.Where(p => p.Category.Id == 1).Sum(p => p.Price);
-            Console.WriteLine("Category 1 Sum prices: " + r12);
-
-            var r13 = products.Where(p => p.Category.Id == 1).Average(p => p.Price);
-            Console.WriteLine("Category 1 Average prices: " + r13);
-
-            var r14 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).DefaultIfEmpty(0.0).Average();
-            Console.WriteLine("Category 5 Average prices: " + r14);
-
-            Console.WriteLine();
-            var r15 = products.Where(p => p.Category.Id == 1).Select(p => p.Price).Aggregate((x, y) => x + y);
-            Console.WriteLine("Category 1 Aggregate Sum: " + r15);
-
-            var r16 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).Aggregate(0.0, (x, y) => x + y);
-            Console.WriteLine("Category 5 Aggregate Sum: " + r16);
-
-            Console.WriteLine();
-            var r17 = products.GroupBy(p => p.Category);
-            foreach(IGrouping<Category, Product> group in r17)
+            //var r17 = products.GroupBy(p => p.Category);
+            var r17 = from p in products group p by p.Category;
+            foreach (IGrouping<Category, Product> group in r17)
             {
                 Console.WriteLine($"Category {group.Key.Name}:");
-                foreach(Product p in group)
+                foreach (Product p in group)
                 {
                     Console.WriteLine(p);
                 }
