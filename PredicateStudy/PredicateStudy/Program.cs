caadb4a648521ee0b7c1201a38690eba22c3ce21
Tiago Barbosa <tiago@farias.cc>
2021-07-28 10:41:41 -0300

    Predicate, exemple with ForEach

--- PredicateStudy/PredicateStudy/Program.cs
+++ PredicateStudy/PredicateStudy/Program.cs
@@ -15,11 +15,43 @@ namespace PredicateStudy
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
+            Action<Product> act = UpdatePrice;
+
+            Console.WriteLine("Using Action:");
+            list.ForEach(act);
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Using extern method");
+            list.ForEach(UpdatePrice);
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Using lambda inline");
+            list.ForEach(p => { p.Price += p.Price * 0.1; });
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine();
+
+
             list.RemoveAll(ProductTest);
             foreach(Product p in list)
             {
                 Console.WriteLine(p);
             }
+        }
+
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
         }
 
         public static bool ProductTest(Product p)
