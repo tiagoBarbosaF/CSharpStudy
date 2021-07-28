e2d25d7019022997711d17c6822602c9888e9c62
Tiago Barbosa <tiago@farias.cc>
2021-07-28 09:43:30 -0300

    Delegate introduction

---
+++ DelegateIntroduction/DelegateIntroduction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DelegateIntroduction.Services;
+
+namespace DelegateIntroduction
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+    delegate double BinaryNumericOperation2(double n1);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op1 = CalculationService.Sum;
+            BinaryNumericOperation op2 = CalculationService.Max;
+            BinaryNumericOperation2 op3 = CalculationService.Square;
+
+            double result1 = op1(a, b);
+            double result2 = op2(a, b);
+            double result3 = op3(a);
+
+            Console.WriteLine($"Sum using delegate => {result1}");
+            Console.WriteLine($"Max using delegate => {result2}");
+            Console.WriteLine($"Square using delegate => {result3}");
+        }
+    }
+}
---
+++ DelegateIntroduction/DelegateIntroduction/Services/CalculationService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DelegateIntroduction.Services
+{
+    class CalculationService
+    {
+        public static double Max(double x, double y)
+        {
+            return (x > y) ? x: y;
+        }
+
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Square(double x)
+        {
+            return Math.Pow(x, 2);
+        }
+    }
+}
