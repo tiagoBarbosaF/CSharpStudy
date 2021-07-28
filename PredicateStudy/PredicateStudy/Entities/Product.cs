dba2346e61fb9045785191b324d47c4be0abe526
Tiago Barbosa <tiago@farias.cc>
2021-07-28 10:29:33 -0300

    Predicate test with RemoveAll

---
+++ PredicateStudy/PredicateStudy/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace PredicateStudy.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} - Price: {Price:F2}";
+        }
+    }
+}
