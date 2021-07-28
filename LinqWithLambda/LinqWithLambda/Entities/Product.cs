33e5ddbb5ca3909a9714c432cdb7e9214f1d0c0d
Tiago Barbosa <tiago@farias.cc>
2021-07-28 13:31:39 -0300

    LINQ with Lambda Part#1

---
+++ LinqWithLambda/LinqWithLambda/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace LinqWithLambda.Entities
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name:{Name}, Price: {Price.ToString("F2", CultureInfo.InvariantCulture)}," +
+                   $" Category: {Category.Name}," +
+                   $" Tier: {Category.Tier}";
+        }
+    }
+}
