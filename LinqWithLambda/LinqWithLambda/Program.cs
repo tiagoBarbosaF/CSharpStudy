deecb229cd62a31a5bd41eb6bf44058aeea05673
Tiago Barbosa <tiago@farias.cc>
2021-07-28 14:38:18 -0300

    LINQ with Lambda Part#2

--- LinqWithLambda/LinqWithLambda/Program.cs
+++ LinqWithLambda/LinqWithLambda/Program.cs
@@ -55,7 +55,7 @@ namespace LinqWithLambda
             var r6 = products.FirstOrDefault();
             Console.WriteLine("First or Default test1: " + r6);
 
-            var r7 = products.Where(p=> p.Price > 3000.0).FirstOrDefault();
+            var r7 = products.Where(p => p.Price > 3000.0).FirstOrDefault();
             Console.WriteLine("First or Default test2: " + r7);
             Console.WriteLine();
 
@@ -64,6 +64,40 @@ namespace LinqWithLambda
 
             var r9 = products.Where(p => p.Id == 30).SingleOrDefault();
             Console.WriteLine("Single or default test2: " + r9);
+
+            var r10 = products.Max(p => p.Price);
+            Console.WriteLine("Max price: " + r10);
+
+            var r11 = products.Min(p => p.Price);
+            Console.WriteLine("Min price: " + r11);
+
+            var r12 = products.Where(p => p.Category.Id == 1).Sum(p => p.Price);
+            Console.WriteLine("Category 1 Sum prices: " + r12);
+
+            var r13 = products.Where(p => p.Category.Id == 1).Average(p => p.Price);
+            Console.WriteLine("Category 1 Average prices: " + r13);
+
+            var r14 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine("Category 5 Average prices: " + r14);
+
+            Console.WriteLine();
+            var r15 = products.Where(p => p.Category.Id == 1).Select(p => p.Price).Aggregate((x, y) => x + y);
+            Console.WriteLine("Category 1 Aggregate Sum: " + r15);
+
+            var r16 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).Aggregate(0.0, (x, y) => x + y);
+            Console.WriteLine("Category 5 Aggregate Sum: " + r16);
+
+            Console.WriteLine();
+            var r17 = products.GroupBy(p => p.Category);
+            foreach(IGrouping<Category, Product> group in r17)
+            {
+                Console.WriteLine($"Category {group.Key.Name}:");
+                foreach(Product p in group)
+                {
+                    Console.WriteLine(p);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
